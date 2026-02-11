80a197381173a55461703180affa873253bc732a
BOLLUNIKHIL <84654902+BOLLUNIKHIL@users.noreply.github.com>
2026-02-11 01:19:23 +0100

    Update exercise2_balanced_symbols.py
    solution ex2

--- LAB_01/exercise2_balanced_symbols.py
+++ LAB_01/exercise2_balanced_symbols.py
@@ -1,1 +1,19 @@
 #exercise2
+
+def is_balanced(s):
+    stack = []
+    pairs = {')': '(', ']': '[', '}': '{'}
+
+
+    for ch in s:
+        if ch in pairs.values():
+            stack.append(ch)
+        elif ch in pairs:
+            if not stack or stack.pop() != pairs[ch]:
+                return False
+
+   
+    return len(stack) == 0
+  
+print(is_balanced("({[]})"))   
+print(is_balanced("([)]")) 
