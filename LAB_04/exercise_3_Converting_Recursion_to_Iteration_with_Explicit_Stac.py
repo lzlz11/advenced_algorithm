072b00a3ee55cf29a7065172c112878f8a6d92ad
onariel <guilhemberin@gmail.com>
2026-03-19 14:27:06 +0100

    exo3 finished

--- LAB_04/exercise_3_Converting_Recursion_to_Iteration_with_Explicit_Stac.py
+++ LAB_04/exercise_3_Converting_Recursion_to_Iteration_with_Explicit_Stac.py
@@ -25,10 +25,12 @@ class thread:
         self.comments.append(comment)
 
 
-def getComments_rec(comment):
-    comments = [comment.content]
+def getComments_rec(comment, comments = None):
+    if comments is None:
+        comments = []
+    comments += [comment.content]
     for replie in comment.getReplies():
-        comments += getComments_rec(replie)
+        getComments_rec(replie, comments)
     return comments
 
 def getComments_it(comment):
@@ -37,9 +39,32 @@ def getComments_it(comment):
     while stack != []:
         curr_comment = stack.pop()
         comments.append(curr_comment.content)
-        for replie in curr_comment.getReplies():
+        for replie in reversed(curr_comment.getReplies()):
             stack.append(replie)
     return comments
+
+def big_node(depth, child, c_main):
+    if depth == 0:
+        return 0
+    for i in range(child):
+        c_next = CommentNone(i, "c" + str(depth) + "_" + str(i), datetime.datetime.now(), i)
+        c_main.addReplies(c_next)
+        big_node(depth - 1, child, c_next)
+    return 0
+
+def test_depth(depth, child, c_main):
+    c_main = CommentNone(1, "c_main", datetime.datetime.now(), 5)
+    big_node(depth, child, c_main)
+
+    start = datetime.datetime.now()
+    getComments_it(c_main)
+    end = datetime.datetime.now()
+    print(f"depth={depth}, child={child} | iterative:  {end - start}")
+
+    start = datetime.datetime.now()
+    getComments_rec(c_main)
+    end = datetime.datetime.now()
+    print(f"depth={depth}, child={child} | recursive: {end - start}")
 
 c1 = CommentNone(1, "c1", datetime.datetime.now(), 1)
 c2 = CommentNone(2, "c2", datetime.datetime.now(), 2)
@@ -52,6 +77,7 @@ c8 = CommentNone(8, "c1_1_1", datetime.datetime.now(), 8)
 c9 = CommentNone(9, "c1_2", datetime.datetime.now(), 9)
 c10 = CommentNone(10, "c1_2_1", datetime.datetime.now(), 10)
 c11 = CommentNone(12, "c1_2_1_1", datetime.datetime.now(), 11)
+c_main = CommentNone(1, "c_main", datetime.datetime.now(), 5)
 
 c1.addReplies(c5)
 c2.addReplies(c6)
@@ -63,3 +89,11 @@ c10.addReplies(c11)
 
 print(getComments_rec(c1))
 print(getComments_it(c1))
+
+test_depth(5, 1, c_main)
+test_depth(10, 1, c_main)
+test_depth(50, 1, c_main)
+test_depth(100, 1, c_main)
+test_depth(500, 1, c_main)
+
+test_depth(20, 2, c_main)
